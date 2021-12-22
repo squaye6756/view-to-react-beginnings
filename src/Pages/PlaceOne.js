c9aa1bfb13fbd64e50b49879907a18b9561c179e
Samuel Quaye <quayes955@gmail.com>
2021-12-21 23:17:14 -0500

    loaded data onto one of the pages

--- src/Pages/PlaceOne.js
+++ src/Pages/PlaceOne.js
@@ -1,8 +1,34 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
 
 const PlaceOne = () => {
+    const [orders, setOrders] = useState([]);
+
+    const loadOrders = () => {
+        axios.get('http://localhost:3000/orders')
+        .then((response) => {
+            setOrders(response.data);
+        });
+    }
+
+    useEffect(() => {
+        loadOrders();
+    }, []);
+
     return (
         <div>
-            <h1>Place One</h1>
+            {orders.map((order) => {
+                return (
+                    <div className='order-individual'>
+                        <p>First Name: {order.CustomerFirstName}</p>
+                        <p>Last Name: {order.CustomerLastName}</p>
+                        <p>Captured Date: {order.CapturedDate}</p>
+                        <p>Id: {order.OrderId}</p>
+                        <p>Total: {order.OrderTotal}</p>
+                        <hr/>
+                    </div>
+                )
+            })}
         </div>
     )
 }
